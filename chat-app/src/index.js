42a469ef6a04d6b5c5c4eee1fb839293ef1d4282
lucamodic <lucanmodic@gmail.com>
2024-01-16 11:27:55 -0300

    first commit

---
+++ chat-app/src/index.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+module.exports = app
+const port = process.env.PORT
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
